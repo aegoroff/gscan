0e431d58a3dce4759d8c37102267f70e82771f95
egoroff <egoroff@gmail.com>
2021-02-12 07:12:09 +0300

    file sizes calculation

--- main.go
+++ main.go
@@ -13,17 +13,19 @@ func main() {
 	start := time.Now()
 	scan.Scan(argsWithoutProg[0], scan.NewOsFs(), h)
 	elapsed := time.Since(start)
-	fmt.Printf("files: %d folders: %d elapsed: %v\n", h.files, h.folders, elapsed)
+	fmt.Printf("files: %d folders: %d size: %d elapsed: %v\n", h.files, h.folders, h.totalSize, elapsed)
 }
 
 type fh struct {
-	files   int64
-	folders int64
+	files     int64
+	folders   int64
+	totalSize int64
 }
 
 func (h *fh) Handle(evt *scan.Event) {
 	if evt.File != nil {
 		h.files += 1
+		h.totalSize += evt.File.Size
 	}
 	if evt.Folder != nil {
 		h.folders += 1
